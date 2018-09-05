c70240fd03b1b761fa1ff42706a43609f93d1d19
Dan Lubbers <lubbers.dan@gmail.com>
2018-09-05 10:51:25 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="animation">
+        <h1>Simple Progress Bar</h1>
+        <div className='bar'></div>
+      </div>
+    );
+  }
+}
+
+export default App;
